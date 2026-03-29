76e8d3262abef210d805dead2a0f98a2c7980095
Jayy <jaysmith.strategist@gmail.com>
2026-03-29 20:15:17 -0300

    Add whitenoise

---
+++ backend/backend/settings.py
@@ -0,0 +1,16 @@
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com"
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
