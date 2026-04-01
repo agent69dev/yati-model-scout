ed55738f00ee0a96fa67e82ac7b9555c3b0aedcf
Jayy <jaysmith.strategist@gmail.com>
2026-04-01 00:13:33 -0300

    fix wsgi.py for correct settings module

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,7 +1,10 @@
 import os
+import sys
+
+# Add the backend folder to Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YatiModelCast.settings')
-
 application = get_wsgi_application()
